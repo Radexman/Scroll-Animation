0eb10fe8ddb550289c1221e2c10e8e25a16dd1f0
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-14 19:36:15 +0100

    Finished main functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello World');
+const mainBox: NodeListOf<Element> = document.querySelectorAll('.main__box');
+
+const checkBoxes = (): void => {
+    const boxTop: number = window.innerHeight / 5 * 4;
+    mainBox.forEach(box => {
+        const boxHeight = box.getBoundingClientRect().top;
+        if (boxHeight < boxTop) {
+            box.classList.add('main__box--active');
+        } else {
+            box.classList.remove('main__box--active');
+        }
+    })
+}
+
+window.addEventListener('scroll', checkBoxes);
