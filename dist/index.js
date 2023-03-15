8f410994d41decdd6c571038eee304787079e5d1
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-03-15 11:06:50 +0100

    Final version

--- dist/index.js
+++ dist/index.js
@@ -1,15 +1,14 @@
-"use strict";
+'use strict';
 const mainBox = document.querySelectorAll('.main__box');
 const checkBoxes = () => {
-    const boxTop = window.innerHeight / 5 * 4;
-    mainBox.forEach(box => {
-        const boxHeight = box.getBoundingClientRect().top;
-        if (boxHeight < boxTop) {
-            box.classList.add('main__box--active');
-        }
-        else {
-            box.classList.remove('main__box--active');
-        }
-    });
+	const boxTop = (window.innerHeight / 5) * 4;
+	mainBox.forEach((box) => {
+		const boxHeight = box.getBoundingClientRect().top;
+		if (boxHeight < boxTop) {
+			box.classList.add('main__box--active');
+		} else {
+			box.classList.remove('main__box--active');
+		}
+	});
 };
 window.addEventListener('scroll', checkBoxes);
